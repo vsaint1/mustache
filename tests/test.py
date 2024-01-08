5472c21d41719b369ca529264670d3b52478449c
vsaint1 <vsantos1.dev@gmail.com>
2024-01-08 01:24:38 -0300

    test: linking and testing on python

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,11 @@ from build import mustache
 
 mm = mustache.Memory("cs2.exe")
 
-module_data = mm.get_module_data("ac_client.dll")
+module_data = mm.get_module_data("client.dll")
 
 print(f"ProcessId: {mm.get_process_id()}")
 print(f"ModuleBase: {module_data[0]} ModuleSize: {module_data[1]}")
 
+local_player = mm.read_ptr(mm.get_module_base() + 0x16C8F58)
+health = mm.read_int(local_player + 0x32C)
+print(f"Health: {health}")
