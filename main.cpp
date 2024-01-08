ef9740fe3890a5612ec2c232b07a2f03abb95030
vsaint1 <vsantos1.dev@gmail.com>
2024-01-08 00:09:08 -0300

    py: memory->process_id api

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
+#include "src/memory/memory.h"
+
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
-float python_sum(float arg1, float arg2) {
-  return arg1 + arg2;
-}
+float python_sum(float arg1, float arg2) { return arg1 + arg2; }
 
-PYBIND11_MODULE(mustache,handle) {
+PYBIND11_MODULE(mustache, handle) {
 
   handle.doc() = "pybind11 example plugin";
-  handle.def("test_sum",&python_sum, "A function which adds two numbers");
+  handle.def("test_sum", &python_sum, "A function which adds two numbers");
+
+  pybind11::class_<Memory>(handle,"Memory")
+      .def(pybind11::init<const std::string_view>(), pybind11::arg("process_name"))
+      .def("get_process_id", &Memory::getProcessId,pybind11::return_value_policy::reference);
+
 }
