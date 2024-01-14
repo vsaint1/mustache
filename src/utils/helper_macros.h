3b7b22b04ea52ef725f4324ffae5809a224ec882
vsaint1 <vsantos1.dev@gmail.com>
2024-01-14 01:06:45 -0300

    idk

--- src/utils/helper_macros.h
+++ src/utils/helper_macros.h
@@ -6,7 +6,7 @@
 #define LOG(fmt, ...)\
 {\
 const auto filename = strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__;\
-printf("[mustache-%s:%d] - " fmt "\n", filename, __LINE__, __VA_ARGS__);\
+printf("[mustache:%s:%d] - " fmt "\n", filename, __LINE__, __VA_ARGS__);\
 }
 #else
 #define LOG(fmt, ...)\
