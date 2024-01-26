5cea475e1b55fc0045d47295238f730d3ad57885
vsaint1 <vsantos1.dev@gmail.com>
2024-01-25 23:04:21 -0300

    feat: mouclass override

---
+++ src/mouse/mouse.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef MOUSE_H
+#define MOUSE_H
+#define ZwClose(handle) NtClose(handle)
+
+
+typedef int BOOL;
+
+BOOL mouse_open(void);
+void mouse_close(void);
+void mouse_move(char button, char x, char y, char wheel);
+void mouse_click();
+#endif
