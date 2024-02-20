cfbb834d3b18617b3ef52de483d8f96e4132ed95
vsaint1 <vsantos1.dev@gmail.com>
2024-02-20 02:37:16 -0300

    handling mouse inputs cross-platform

--- src/mouse/mouse.h
+++ src/mouse/mouse.h
@@ -1,13 +1,59 @@
-#pragma once
 #ifndef MOUSE_H
 #define MOUSE_H
-#define ZwClose(handle) NtClose(handle)
+
+#define MOUSE_PRESS 1
+#define MOUSE_RELEASE 2
+#define MOUSE_MOVE 3
+#define MOUSE_CLICK 4
+
+#if WIN32
+
+#include <Windows.h>
+
+#endif
+
+#include <SDL.h>
+
+class Mouse {
+
+    int m_mouseX;
+    int m_mouseY;
+
+public:
 
 
-typedef int BOOL;
+    explicit Mouse() {
+        SDL_GetGlobalMouseState(&m_mouseX, &m_mouseY);
+    }
 
-BOOL mouse_open(void);
-void mouse_close(void);
-void mouse_move(char button, char x, char y, char wheel);
-void mouse_click();
+    void move(unsigned long x, unsigned long y) {
+#if WIN32
+
+        SDL_MouseMotionEvent(SDL_MOUSEMOTION, 0, 0, 0, 0, x, y, 0, 0);
+
+#endif
+        mouse_event(0x0001, x, y, 0, 0);
+    }
+
+    void click() {
+#if WIN32
+
+        mouse_event(0x0002, 0, 0, 0, 0);
+        mouse_event(0x0004, 0, 0, 0, 0);
+
+#endif
+        SDL_MouseMotionEvent(SDL_BUTTON_LMASK, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+
+    int getMouseX() const {
+        return m_mouseX;
+    }
+
+    int getMouseY() const {
+        return m_mouseY;
+    }
+
+
+};
+
 #endif
