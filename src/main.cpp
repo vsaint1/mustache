5b3bd9cad9c8a3d79a8602874b05a86dc2e45dac
vsaint1 <vsantos1.dev@gmail.com>
2024-03-11 02:20:38 -0300

    export dll

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,9 @@
 #include "math/math.h"
-#include <stdio.h>
+#include "utils/helper_macros.h"
+#include "window/window_manager.h"
 
-#if PYTHON_BINDING
-
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-
-void hello() {
-  
-  printf("Hello, World!\n");
-
+MUSTACHE_API bool create_window(const char *name, bool monitor_size, bool vsync) {
+  static WindowManager window;
+  bool result = window.create(name, monitor_size, vsync);
+  return result;
 }
-
-PYBIND11_MODULE(mustache, handle) {
-
-    handle.doc() = "OS library for memory access, Window management and overlay drawing.";
-
-  handle.def("say_hello", &hello, "Hello, World!");
-}
-
-#endif
-
-#if JAVA_BINDING
-#include "memory/memory.h"
-#include <jni.h>
-
-extern "C" {
-
-JNIEXPORT long JNICALL Java_com_vsantos1_getProcessId(JNIEnv *p_env, jobject jobj) {
-
-  // TODO: just dev purpose
-  long pid = 12345;
-  SPDLOG_INFO("Process id: {}", pid);
-  return static_cast<jlong>(pid);
-}
-}
-
-#endif
-
-#if MUSTACHE_SHARED
-#include "memory/memory.h"
-
-__declspec(dllexport) unsigned int getProcessId() {
-  // TODO: just dev purpose
-  long pid = 12345;
-  SPDLOG_INFO("Process id: {}", pid);
-  return pid;
-}
-
-#endif
