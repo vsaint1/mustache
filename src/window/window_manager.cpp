73b391590ba55dc3bbababf8ba0132b81a016221
vsaint1 <vsantos1.dev@gmail.com>
2024-03-11 03:31:47 -0300

    feat: mouse_click_throuh and window target

--- src/window/window_manager.cpp
+++ src/window/window_manager.cpp
@@ -24,6 +24,12 @@ bool WindowManager::create(const char *name_, bool monitor_size, bool vsync) {
     this->m_height = mode->height;
   } else {
     const auto window_size = get_target_window_size(name_);
+
+    if(window_size.first == 0 || window_size.second == 0) 
+      return false;
+    
+    this->m_width = window_size.first;
+    this->m_height = window_size.second;
   }
 
   if (monitor_size) {
@@ -31,6 +37,8 @@ bool WindowManager::create(const char *name_, bool monitor_size, bool vsync) {
   } else {
     this->m_window = glfwCreateWindow(this->m_width, this->m_height, RANDOM_NAME(16).c_str(), NULL, NULL);
   }
+
+  SPDLOG_INFO("Window created, sizes: {}x{}", this->m_width, this->m_height);
 
   if (!this->m_window)
     return false;
@@ -57,15 +65,21 @@ void WindowManager::render() {
 
 void WindowManager::pool_events() { glfwPollEvents(); }
 
+void WindowManager::mouse_click_through(bool enable) { glfwWindowHint(GLFW_MOUSE_PASSTHROUGH, enable ? GLFW_TRUE : GLFW_FALSE); }
+
 std::pair<float, float> WindowManager::get_target_window_size(const char *window_name) {
 
 #if _WIN32
   HWND hwnd = FindWindowA(NULL, window_name);
-  if (hwnd == NULL)
+  if (hwnd == NULL) {
+    throw std::runtime_error::exception("Window not found");
     return {};
+  }
 
   RECT rect;
   GetWindowRect(hwnd, &rect);
+
+  SPDLOG_INFO("Window found, sizes: {}x{}", rect.right - rect.left, rect.bottom - rect.top);
 
   return {rect.right - rect.left, rect.bottom - rect.top};
 #else
--- src/window/window_manager.h
+++ src/window/window_manager.h
@@ -7,7 +7,7 @@
 #include "GLFW/glfw3.h"
 #include <iostream>
 #include "../utils/helper_macros.h"
-
+#include <stdexcept>
 #if _WIN32
 #include <windows.h>
 #endif
@@ -16,6 +16,7 @@ class WindowManager {
   int m_width;
   int m_height;
   GLFWwindow *m_window;
+  bool m_transparent = false;
 
 public:
   bool create(const char *name_, bool monitor_size = true, bool vsync = true);
@@ -32,6 +33,7 @@ public:
 
   GLFWwindow *get_window() const { return m_window; }
 
+  void mouse_click_through(bool enable);
 
 private:
  std::pair<float,float> get_target_window_size(const char* window_name);
