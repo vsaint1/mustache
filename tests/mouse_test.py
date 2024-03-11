1e64c015d0e48b4053915505eae520cf375b8cbe
vsaint1 <vsantos1.dev@gmail.com>
2024-03-11 02:20:25 -0300

    rm: old tests

--- tests/mouse_test.py
+++
@@ -1,8 +0,0 @@
-from build import mustache
-
-mouse = mustache.Mouse()
-
-while True:
-    print(f"Mouse position {mouse.get_pos_x()}, {mouse.get_pos_y()}\n")
-    if mustache.key_pressed(0x05):
-        mouse.move(100,200)
